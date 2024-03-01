39f9fab27324eaa18824b6873ab05a63a9566237
ChilledzoneTechnologies <narteytettehe@gmail.com>
2024-02-29 19:58:59 -0800

    binary_tree project

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,29 @@
+[4:58 pm, 29/02/2024] Ee: 16-binary_tree_is_perfect.c
+[4:58 pm, 29/02/2024] Ee: #include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+#include "9-binary_tree_height.c"
+#include "11-binary_tree_size.c"
+
+/**
+ * binary_tree_is_perfect - checks if a tree is a perfect tree
+ *
+ * @tree: the root node
+ * Return: 1 if perfect 0 otherwise
+ */
+
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t size = 0;
+	size_t height = 0;
+	size_t perfect = 0;
+
+	if (!tree)
+		return (0);
+	height = binary_tree_height(tree);
+	size = binary_tree_size(tree);
+	perfect = (size == 1 ? 1 : (1 << (height + 1)) - 1);
+
+	return (size == perfect ? 1 : 0);
+}
